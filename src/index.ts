da55f204dfc3737cb6f3b129e933ec5e5df2ba58
Kostya Kostyanitsa <kostyanitsakonstantyn@gmail.com>
2020-07-01 21:07:25 +0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express = require('express');
 import config from '../config';
 import api from './features';
 import './db';
+const cors = require("cors");
 
 
 const app = express();
 
-app.use('/api', api)
+app.use(cors());
 
+app.use('/api', api)
 app.listen(config.port, err=>{
     if(err)
     {
